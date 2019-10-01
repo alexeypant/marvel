344a98557aa3a468703849144b774c3a8bf4c455
alexeypant <alexeypant@gmail.com>
2019-10-01 09:50:45 +0300

    MRVL-001-add-table

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {CharactersTable} from "./CorePackage/TableModule/components/CharactersTable";
+
+const dataSource = [
+  {
+    key: '1',
+    name: 'Mike',
+    age: 32,
+    address: '10 Downing Street',
+  },
+  {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street',
+  },
+  {
+    key: '3',
+    name: 'Mary',
+    age: 52,
+    address: '30 Downing Street',
+  },
+  {
+    key: '4',
+    name: 'Caty',
+    age: 25,
+    address: '14 Downing Street',
+  },
+];
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CharactersTable data={dataSource} columns={columns} onNextClick={() => {}} onPreviousClick={()=>{}}/>
     </div>
   );
-}
+};
 
 export default App;
