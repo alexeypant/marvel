c3579b56b3823519c9abf16c020f5561b6babe13
alexeypant <alexeypant@gmail.com>
2019-10-09 17:18:15 +0300

    add server and client folders

---
+++ server/src/main.ts
@@ -0,0 +1,15 @@
+import * as express from 'express';
+import * as http from 'http';
+
+
+const PORT = 3000;
+const app = express();
+// app.use('/',             express.static(path.join(__dirname, '../client/dist')));
+// app.use('/node_modules', express.static(path.join(__dirname, '../../node_modules')));
+
+const httpServer: http.Server = app.listen(PORT, () => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(`Listening on http://localhost:${PORT}`);
+  }
+});
+
