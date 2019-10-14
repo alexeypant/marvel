19d3692e70f9c36a42c01ddb761b450c58cf0c87
alexeypant <alexeypant@gmail.com>
2019-10-14 22:52:49 +0300

    add about page

--- client/src/components/about/About.tsx
+++ client/src/components/about/About.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import './About.css';
 
 export const About = () => (
-  <div>
-    there will be description of the website here
+  <div className="word">
+    <span className="letter m">M</span>
+    <span className="letter a">A</span>
+    <span className="letter r">R</span>
+    <span className="letter v">V</span>
+    <span className="letter e">E</span>
+    <span className="letter l">L</span>
   </div>
 );
