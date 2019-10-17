7791cea89ce788938fd26c1055b0763b9146828e
alexeypant <alexeypant@gmail.com>
2019-10-17 11:07:28 +0300

    fixes redirects in LoginFormContainer

--- client/src/containers/loginForm/LoginFormContainer.tsx
+++ client/src/containers/loginForm/LoginFormContainer.tsx
@@ -5,14 +5,16 @@ import { State } from '../../store/types/State';
 import { loginClearState, loginRequest } from '../../store/actions/login/actionCreators';
 import { LoginData } from '../../types/users/LoginData';
 import { initialLoginData } from './constants';
-import { useHistory } from 'react-router';
+import { useHistory, useLocation } from 'react-router';
 import { ERoute } from '../../enums/Route';
 
 export const LoginFormContainer = () => {
   const [loginData, setLoginData] = useState<LoginData>(initialLoginData);
   const { isLoginPending, isLoggedIn, error } = useSelector(state => (state as State).login);
-  const history = useHistory();
   const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+  const { from } = location.state || { from: { pathname: ERoute.root } };
 
   const handleChange = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => {
@@ -37,11 +39,11 @@ export const LoginFormContainer = () => {
     () => {
       if (isLoggedIn) {
         setLoginData(initialLoginData);
-        history.push(ERoute.root);
+        history.replace(from);
         dispatch(loginClearState());
       }
     },
-    [isLoggedIn, setLoginData, history, dispatch],
+    [isLoggedIn, setLoginData, from, history, dispatch],
   );
 
   useEffect(() => {
